d44679e9b7f8cfb8407aaa8db00f8538e7a43d43
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-07-18 16:44:09 +0100

    Allow configuration custom package dir for Python library

--- scripts/crit-setup.py
+++ scripts/crit-setup.py
@@ -1,5 +1,11 @@
 from distutils.core import setup
+import os
 
+CUSTOM_PKG_DIR = os.environ["PY_PKG_DIR"]
+DEFAULT_PKG_DIR = "pycriu"
+
+if CUSTOM_PKG_DIR is None or CUSTOM_PKG_DIR == "":
+    CUSTOM_PKG_DIR = DEFAULT_PKG_DIR
 
 setup(name = "crit",
       version = "0.0.1",
@@ -7,8 +13,8 @@ setup(name = "crit",
       author = "CRIU team",
       author_email = "criu@openvz.org",
       url = "https://github.com/xemul/criu",
-      package_dir = {'pycriu': 'lib/py'},
-      packages = ["pycriu", "pycriu.images"],
+      package_dir = {CUSTOM_PKG_DIR: 'lib/py'},
+      packages = [CUSTOM_PKG_DIR, CUSTOM_PKG_DIR + ".images"],
       package_data={'': ['templates/*.tmpl']},
       scripts = ["crit/crit"]
       )
